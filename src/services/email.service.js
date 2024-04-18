4803d70902705976d4f4a99d71a3415e50cc3108
sordgom <aymaneigmiden@gmail.com>
2024-04-18 18:33:32 +0100

    Email implemetation #1

---
+++ src/services/email.service.js
@@ -0,0 +1,12 @@
+// TODO: Implement email service
+const sendResetPasswordEmail = (email ,token) => {
+
+};
+
+const sendVerificationEmail = (email, token) => {
+
+};
+
+module.exports = {
+    sendResetPasswordEmail, sendVerificationEmail
+};
